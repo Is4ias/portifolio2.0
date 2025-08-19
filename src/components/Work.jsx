a0c638e5990cdb32d146a49dd59527a1412c27bd
Is4ias <isaias02dev@gmail.com>
2025-08-19 18:32:23 -0300

    feat: adicionando componente WORK/Projetos

---
+++ src/components/Work.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import ProjectCard from './projectCard';
+import Foto from '../assets/images/download.jpg'
+import Cafe from '../assets/images/cafeWallpaper.jpg'
+import cars from '../assets/images/carsApp.jpg';
+import Cotacao from '../assets/images/CotacoesApp.jpg';
+import FastAPI from '../assets/images/PythonFastAPI.jpg';
+import Ecomerce from '../assets/images/ecomerce.jpg';
+import ChatBot from '../assets/images/ChatBot.jpg';
+
+
+const works = [
+  {
+    imgSrc: Cafe,
+    title: 'Ribeiro`S - Full stack  App',
+    tags: ['React Native','Desenvolvimento Moblie', 'API'],
+    projectLink: 'https://musify-5al0.onrender.com/'
+  },
+  {
+    imgSrc: Cotacao,
+    title: 'Cotaçoes App',
+    tags: ['Python', 'Streamlit', 'Pandas'],
+    projectLink: 'https://pixstock-official.vercel.app/'
+  },
+  {
+    imgSrc: cars,
+    title: 'DriveUP',
+    tags: ['Desenvolvimento Moblie', 'API'],
+    projectLink: ''
+  },
+  {
+    imgSrc: FastAPI,
+    title: 'To-do-List',
+    tags: ['Python', 'FastAPI', 'Swagger '],
+    projectLink: 'https://github.com/codewithsadee-org/wealthome'
+  },
+  {
+    imgSrc: Ecomerce,
+    title: 'Compre Mais',
+    tags: ['eCommerce', 'Django'],
+    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+  },
+  {
+    imgSrc: ChatBot,
+    title: 'Vivy - Agente pessoal de saúde',
+    tags: ['Kotlin', 'SQL'],
+    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+  },
+  
+];
+
+
+
+const Work = () => {
+  return (
+    <section
+      id='work'
+      className='section'
+      >
+        <div className="container">
+          <h2 className="headline-2 mb-8">
+            Meus projetos
+          </h2>
+
+          <div className="grid gap-x-4 gap-y-5 
+          grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+            {works.map(({ imgSrc, title, tags, projectLink }, 
+            key) => (
+              <ProjectCard
+                key={key}
+                imgSrc={imgSrc}
+                title={title}
+                tags={tags}
+                projectLink={projectLink}
+
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default Work
