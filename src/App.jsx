7f096de39a36fb78205b3128d6a9746d36ea23ba
Is4ias <isaias02dev@gmail.com>
2025-09-09 22:42:40 -0300

    feat: adiciona smooth scroll e animações com GSAP ScrollTrigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { ReactLenis } from 'lenis/react'; 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -9,8 +16,28 @@ import Footer from "./components/Footer";
 
 
 const App = () => {
+
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: '-200 bottom',
+          end: 'bottom 80%',
+          scrub: true,
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease:'power.out'
+      })
+    })
+  });
+
   return(
-    <>
+    <ReactLenis>
     <Header/>
     <main>
       <Hero/>
@@ -21,7 +48,7 @@ const App = () => {
       <Contact/>
     </main>
     <Footer/>
-    </>
+    </ReactLenis>
   )
   
 }
